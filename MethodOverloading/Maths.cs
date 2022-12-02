27fc61e37c8142229e4df747bd182d12cc6934ae
Nick Carlson <CarlsonNicholas82@gmail.com>
2022-12-02 12:01:02 -0500

    Method Overloading Exercise

---
+++ MethodOverloading/Maths.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Maths
+    {
+        public static int Add(int num1, int num2)
+        {
+            return(num1 + num2);
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return(num1 + num2);
+        }
+        public static string Add(int num1, int num2, bool check)
+        {
+            var solution = num1 + num2;
+
+            if (check == true && solution > 1)
+            {
+                return $"{solution} dollars";
+            }
+            else if (check == true && solution == 1)
+            {
+                return $"{solution} dollar";
+            }
+            else if (check == true && solution < 1)
+            {
+                return $"{solution} dollars";
+            }
+            else
+            {
+                return solution.ToString();
+            }
+
+        }
+
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int partOne = Maths.Add(1, 2);
+            decimal partTwo = Maths.Add(1.1m, 2.2m);
+           string partThree =  Maths.Add(2, 3, true);
+
+            Console.WriteLine(partOne);
+            Console.WriteLine("");
+            Console.WriteLine(partTwo);
+            Console.WriteLine("");
+            Console.WriteLine(partThree);
+
         }
     }
 }
